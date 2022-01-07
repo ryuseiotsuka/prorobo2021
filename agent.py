ecd97b5a7ab668ea6ba9a761a2862c1f2e229fc7
RYUSEI OTSUKA <50820803+ryuseiotsuka@users.noreply.github.com>
2022-01-07 20:11:01 +0900

    Update code

--- agent.py
+++ agent.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# ### Agent
+# ここでは学習対象の行動を反映するを記述しています．
+
+# ### インポートするモジュール
+# ここでは迷路部分を実装しているMapをインポートしています
+
 # In[ ]:
 
 
-import math
 from map import Map 
+
+
+# ### Agent　行動の反映
+# ここではAgentがどのように行動するのか，周囲の情報をどのように得ているのかについて記述しています．
+# 
+# -----
+# * インスタンス化　1-6行目\
+# ここではクラスをインスタンス化しています．また，スタートとゴールの位置，自己位置の初期値，行動空間について初期化を行っています．
+# 自己位置はスタート地点と同じになるようにしています．
+# 
+# -----
+# * 行動　7-17行目\
+# ここでは，自分がマス目を移動するためのコードが書かれています．移動先に壁がないことを確認して移動しています．
+# 
+# -----
+# * 周囲の情報の獲得　19-30行目\
+# ここでは学習を行う際に必要な自己位置の情報を得る部分について記述しています．
+# 参考のプログラムと異なる点は，自己位置の周囲1マス分しか得ていない点とゴールまでの距離を点数化したものを情報として与えている点です．\
+# 参考としたプログラムでは，自己位置がわかっている状態で学習を進めていました．私のプログラムでは自己位置の情報が限定されても学習を行うことができるようにしたかったため周囲1マス分，計8マスの情報だけを与えるようにしました．これは自分の位置から見て周囲1マスそれぞれに壁があるかどうかを見ることで実現しています．matplotlibの描画と地図を生成する配列の向きが異なる都合で，それぞれの位置を表す1番目の配列は11から引くようにしています．\
+# また，これに追加してゴールまでの距離を点数化し足し合わせています．これは三平方の定理によってゴールまでの距離を求めているのではなく，縦と横の距離が最大10であることを利用し，周囲1マスの情報と同じ変数内で距離を記述できるよう工夫しています．\
+# このStateが最大で25856となるため，Q-leaningのQ-tableの大きさが25856となっています．
+# 
+# -----
+# * ゴールチェック　33-38行目\
+# 現在の位置がゴールの位置と重なった際にdoneにTruthを返すようになっています．
+
+# In[ ]:
+
 
 class Agent(object):
     def __init__(self,init_pos = [1,1],goal_pos = [10,10]):
@@ -26,9 +59,8 @@ class Agent(object):
             pass
     
     def get_state(self,_map):
-        #self.goaldis = round(math.sqrt((self.goal_pos[0]-self.pos[0])**2)+math.sqrt((self.goal_pos[1]-self.pos[1])**2))
-        #state = self.pos[0]*12 + self.pos[1]　右上から反時計回り
-        state = (self.goal_pos[0]-self.pos[0])*10+(self.goal_pos[1]-self.pos[1])*2**8+                2**7*bool(_map[11-self.pos[0]+1][self.pos[1]+1])+                2**6*bool(_map[11-self.pos[0]+0][self.pos[1]+1])+                2**5*bool(_map[11-self.pos[0]-1][self.pos[1]+1])+                2**4*bool(_map[11-self.pos[0]-1][self.pos[1]+0])+                2**3*bool(_map[11-self.pos[0]-1][self.pos[1]-1])+                2**2*bool(_map[11-self.pos[0]+0][self.pos[1]-1])+                2**1*bool(_map[11-self.pos[0]+1][self.pos[1]-1])+                2**0*bool(_map[11-self.pos[0]+1][self.pos[1]+0])
+        #右上から反時計回り
+        state = (self.goal_pos[0]-self.pos[0])*2**9+                (self.goal_pos[1]-self.pos[1])*2**8+                2**7*bool(_map[11-self.pos[0]+1][self.pos[1]+1])+                2**6*bool(_map[11-self.pos[0]+0][self.pos[1]+1])+                2**5*bool(_map[11-self.pos[0]-1][self.pos[1]+1])+                2**4*bool(_map[11-self.pos[0]-1][self.pos[1]+0])+                2**3*bool(_map[11-self.pos[0]-1][self.pos[1]-1])+                2**2*bool(_map[11-self.pos[0]+0][self.pos[1]-1])+                2**1*bool(_map[11-self.pos[0]+1][self.pos[1]-1])+                2**0*bool(_map[11-self.pos[0]+1][self.pos[1]+0])
         return state 
 
     def check_done(self):
--- map.py
+++ map.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# ### Map
+# ここでは迷路に必要な地図を生成するためのプログラムを記述しています．
+
+# ### インポートするモジュール
+# 地図を表示したり，前の地図を消すために必要なdisplay, clear_outputと地図の表示を行うmatplotlib，数値計算を高速化するnumpyをインポートしています．
+
 # In[ ]:
 
 
-from ipywidgets import FloatProgress
 from IPython.display import display, clear_output
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# ### 地図の生成
+# -----
+# * インスタンス化　1-27行目\
+# 3-6行目ではクラスのインスタンス化とスタートとゴールの座標，迷路となる配列の生成，マップサイズの計算を行っています．\
+# 参考のプログラムと異なる点は，地図となる配列の生成をランダムにしたところです．迷路のもととなる12×12の配列の各要素に0以上101未満の100種類の数字をランダム割り当てるようにしました．\
+# 8-18行目では迷路を生成するための配列の各要素のどこが壁でどこがなにもない空間かを決めるためのしきい値を設定し、それに応じて配列を変えています．\
+# 16-19行目では迷路の四辺が壁で囲まれるようにするための処理を行っています．迷路の四辺は必ず配列の0番目または11番目なのでこれを用いて条件分岐を行っています．
+# 21-26行目ではスタート地点とゴール地点の設定と，それぞれが壁に埋まらないように処理を行っています．また，迷路の表示サイズの設定を行っています．\
+# 
+# -----
+# * 当たり判定　28-33行目\
+# ここでは上下左右に移動可能かをboolで判断しています．壁があるところの配列が0となっているためFaithが返ってくるとそこに行くことができないと判断します．\
+# 
+# -----
+# * 迷路の表示　35-54行目\
+# ここでは主にMatplotlibを利用し迷路を表示しています．36-38行目では迷路を生成する配列のサイズからマス目の数を計算し縦横に線を引いています．\
+# 39-50行目では各マスのうち，壁，スタート地点，ゴール地点，自己位置を塗る処理をしています．
+
+# In[ ]:
+
 
 class Map(object):
     def __init__(self):
@@ -35,24 +60,20 @@ class Map(object):
         self.goal_pos = [10,10]
         self.map[11-self.init_pos[0]][self.init_pos[1]] = 2
         self.map[11-self.goal_pos[0]][self.goal_pos[1]] = 3
-        plt.ion()
+        plt.ion()# matplotlibでプロットの更新を自動化
         self.fig = plt.figure(figsize=(7,7))
-        self.ax = self.fig.add_subplot(111)
 
     def chack_movable(self,pos):
         up    = bool(self.map[11-pos[1]-1][pos[0]])
         down  = bool(self.map[11-pos[1]+1][pos[0]])
         right = bool(self.map[11-pos[1]][pos[0]+1])
         left  = bool(self.map[11-pos[1]][pos[0]-1])
-
         return([up,down,right,left])
 
     def plot(self,pos=[1,1],q_table=[]):
         gpoint = np.arange(0, self.size, 1)
         plt.vlines(gpoint, 0, self.size, linewidth=0.3, colors="k")
         plt.hlines(gpoint, 0, self.size, linewidth=0.3, colors="k")
-        plt.xlim(0, self.size)
-        plt.ylim(0, self.size)
         for i in range (self.size):
             for j in range (self.size):
                 if self.map[11-j][i] == 0:
